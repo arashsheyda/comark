c72799f869fec6d1687b9f1547283a178ca6acfd
Farnabaz <farnabaz@gmail.com>
2026-02-23 17:39:10 +0100

    feat: react `defineComarkComponent`

--- packages/comark/src/react/index.ts
+++ packages/comark/src/react/index.ts
@@ -1,2 +1,66 @@
+import React from 'react'
+import { Comark } from './components/Comark'
+import type { ComarkProps } from './components/Comark'
+import type { ParseOptions } from '../types'
+
 export { ComarkRenderer } from './components/ComarkRenderer'
-export { Comark } from './components/Comark'
+export { Comark }
+
+interface DefineComarkComponentOptions extends ParseOptions {
+  name?: string
+  components?: Record<string, React.ComponentType<any>>
+}
+
+/**
+ * Create a pre-configured Comark component with default options, plugins, and components.
+ *
+ * @example
+ * ```tsx
+ * import { defineComarkComponent } from 'comark/react'
+ * import comarkMath from '@comark/math'
+ * import { Math } from '@comark/math/react'
+ *
+ * export const AppComark = defineComarkComponent({
+ *   name: 'AppComark',
+ *   plugins: [comarkMath()],
+ *   components: { Math },
+ *   highlight: {
+ *     themes: {
+ *       light: 'github-light',
+ *       dark: 'github-dark',
+ *     },
+ *   },
+ * })
+ * ```
+ */
+export function defineComarkComponent(config: DefineComarkComponentOptions = {}) {
+  const { name, components: configComponents = {}, ...parseOptions } = config
+
+  const ComarkComponent: React.FC<ComarkProps> = (props) => {
+    // Merge options
+    const mergedOptions: ParseOptions = {
+      ...parseOptions,
+      ...props.options,
+      plugins: [
+        ...(config.plugins || []),
+        ...(props.options?.plugins || []),
+      ],
+    }
+
+    // Merge components (props override config)
+    const mergedComponents = {
+      ...configComponents,
+      ...props.components,
+    }
+
+    return React.createElement(Comark, {
+      ...props,
+      options: mergedOptions,
+      components: mergedComponents,
+    })
+  }
+
+  ComarkComponent.displayName = name || 'ComarkComponent'
+
+  return ComarkComponent
+}
