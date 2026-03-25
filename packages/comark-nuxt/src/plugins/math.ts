caee05ee50e0af5e1412186e80f449f650750bc2
Farnabaz <farnabaz@gmail.com>
2026-03-25 17:50:28 +0100

    fix(nuxt): re-export Math & Mermaid plugins from Vue package

---
+++ packages/comark-nuxt/src/plugins/math.ts
@@ -0,0 +1,2 @@
+export * from '@comark/vue/plugins/math'
+export { default } from '@comark/vue/plugins/math'
---
+++ packages/comark-nuxt/src/plugins/mermaid.ts
@@ -0,0 +1,2 @@
+export * from '@comark/vue/plugins/mermaid'
+export { default } from '@comark/vue/plugins/mermaid'
